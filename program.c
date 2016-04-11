9edc56885af215a349a871cebd1bf3d0b579e582
mareczekk <mar9600.mz@gmail.com>
2016-04-11 19:07:05 +0200

    program.c

---
+++ program.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+ struct adres_bajtowy {
+  __uint8_t a;
+  __uint8_t b;
+  __uint8_t c;
+  __uint8_t d;
+  };
+ 
+ union adres {
+   __uint32_t ip;
+   struct adres_bajtowy badres;
+   };
+ 
+ int main ()
+ {
+    union adres addr;
+    addr.badres.a = 192;
+    addr.badres.b = 168;
+    addr.badres.c = 1;
+    addr.badres.d = 1;
+    printf ("Adres IP w postaci 32-bitowej zmiennej: %08x\n",addr.ip);
+    return 0;
+ for(a;b;++c)
+ to = malloc(sizeof(int**))
+ 
+ t = zombie.find(1)
+ 
+ return 0;
+ }
